5d5841d045010cfb7b3e438ad5033801573b5c45
Pratham6124 <prathamgautam153@gmail.com>
2026-02-02 00:49:52 +0530

    Add moveNo function to randomize button position

--- script.js
+++ script.js
@@ -7,6 +7,12 @@ if(pass !== "pizza") {
 function goYes() {
   window.location.href = "yes.html";
 }
+function moveNo() {
+  let btn = document.getElementById("noBtn");
+  btn.style.position = "absolute";
+  btn.style.left = Math.random() * 80 + "%";
+  btn.style.top = Math.random() * 80 + "%";
+}
 function showLove() {
   alert("Will you be my Valentine? 💖");
 }
