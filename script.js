1ca9e8e24508fd2873a6ee79d2d7995bd7d03c09
Pratham6124 <prathamgautam153@gmail.com>
2026-02-02 00:42:17 +0530

    Add goYes function to redirect to yes.html

--- script.js
+++ script.js
@@ -4,7 +4,9 @@ if(pass !== "pizza") {
   alert("Wrong password 😝");
   window.location.href = "https://google.com";
 }
-
+function goYes() {
+  window.location.href = "yes.html";
+}
 function showLove() {
   alert("Will you be my Valentine? 💖");
 }
