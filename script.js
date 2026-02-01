89052499dc1e6ca186baccfd37dcf0a375037c54
Pratham6124 <prathamgautam153@gmail.com>
2026-02-02 00:06:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+let password = prompt("Enter the secret word 💕");
+
+if (password !== "pizza") {
+  alert("Wrong password 😝");
+  window.location.href = "https://google.com";
+}
+
+function showLove() {
+  alert("Will you be my Valentine? 💖");
+}
